922023dccc55a6373f368bde129d296fd7772238
Mudit <muditert34@gmail.com>
2023-07-30 00:05:22 +0530

    added api's

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 const { Eta } = require("eta");
 const path = require("path");
 const fs = require("fs");
+const axios = require('axios');
 
-const eta = new Eta({ views: path.join(__dirname, "templates") });
+const API_URL = 'https://www.boredapi.com/api/activity';
+const PAGES_COUNT = 3;
 
 // Render the template
-const res = eta.render("./simple", { name: "Ben" });
-const outputFilePath = path.join(__dirname, "output.html");
+const eta = new Eta({ views: path.join(__dirname, "templates") });
 
-fs.writeFileSync(outputFilePath, res);
+async function fetchDataFromApi() {
+    try {
+        const response = await axios.get(API_URL);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching data from the API:', error);
+        return [];
+    }
+}
 
-console.log("HTML content has been written to output.html"); 
+async function fetchDataAndProcess() {
+    for (let i = 0; i < PAGES_COUNT; i++) {
+        try {
+            const data = await fetchDataFromApi();
+            console.log(data);
+            const res = eta.render("./simple", { name: `${data.activity}` });
+            const outputFilePath = path.join(__dirname, `output${i}.html`);
+            fs.writeFileSync(outputFilePath, res);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+}
+
+fetchDataAndProcess();
